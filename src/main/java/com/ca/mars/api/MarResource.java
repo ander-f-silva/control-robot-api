e436e393cc7a27783f5fc75c6370565e03f4c439
Anderson Silva <anderson.silva@concrete.com.br>
2017-08-24 17:13:54 -0300

    Mudança na versão do spring boot

--- src/main/java/com/ca/mars/api/MarResource.java
+++ src/main/java/com/ca/mars/api/MarResource.java
@@ -7,11 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,7 +19,6 @@ public class MarResource {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-
     private final Robot robot;
 
     @Autowired
@@ -30,18 +27,18 @@ public class MarResource {
     }
 
     @PostMapping(value = "/{commands}")
-    public ResponseEntity<Position> toIdentifyRobotPosition(@PathVariable("commands") String commands) {
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Position> toIdentifyRobotPosition(@PathVariable("commands") String commands) {
         logger.info("Starting the query of the coordinates of the robot!");
-        return ResponseEntity.ok(robot.move(commands));
+        return Mono.just(robot.move(commands));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleIllegalArgumentException(HttpServletRequest request, Exception ex) {
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
         logger.error("Command Invalid!");
         Map<String, String>  messages = new LinkedHashMap<>();
         messages.put("error",ex.getMessage());
         return messages;
     }
-
 }
